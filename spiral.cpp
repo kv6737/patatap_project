9ea3d0a15fa0eae9c829741891fce2df1befbdb6
kv6737 <48996208+kv6737@users.noreply.github.com>
2019-08-08 03:51:44 +0530

    Add files via upload

---
+++ spiral.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[100][100],i,j;
+	cin>>n;
+	for(int i=0;i<n;i++)
+    {
+    	for(int j=0;j<n;j++)
+	    {
+	    	cin>>arr[i][j];
+
+	    }
+    }
+    for( i=0;i<n;i++)
+    {
+    	while()
+    }
+    return 0;
+
+}
